dc25fedac20f6249b14974e66f5d70c85163072b
kc <kc.dark@protonmail.com>
2021-08-06 09:24:15 +0530

    Bonus

--- task2/server.js
+++ task2/server.js
@@ -1,4 +1,5 @@
 // Importing modules
+const { response } = require('express');
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
@@ -9,9 +10,21 @@ app.listen(3000, () => {
 });
 
 // Get request 
-app.get("/", async (req, res) => {
+app.get("/breakingbad", async (req, res) => {
     // Fetching the data
-    const response = await fetch("https://www.breakingbadapi.com/api/characters");
+    const response = await fetch("https://www.breakingbadapi.com/api/characters?category=Breaking+Bad");
+
+    // Parsing the data in json format
+    const data = await response.json();
+
+    // Sending a response
+    res.send(data);
+});
+
+// Bonus
+app.get("/brettercallsaul", async (req, res) => {
+    // Fetching the data
+    const response = await fetch("https://www.breakingbadapi.com/api/characters?category=Better+Call+Saul");
 
     // Parsing the data in json format
     const data = await response.json();
