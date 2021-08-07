1f9b75851c7d7be8776a093685aa8e17f97daa55
kc <kc.dark@protonmail.com>
2021-08-08 00:31:31 +0530

    Added error handeling

--- task2/server.js
+++ task2/server.js
@@ -11,23 +11,41 @@ app.listen(3000, () => {
 
 // Get request 
 app.get("/breakingbad", async (req, res) => {
-    // Fetching the data
-    const response = await fetch("https://www.breakingbadapi.com/api/characters?category=Breaking+Bad");
+    try{
+        // Fetching the data
+        const response = await fetch("https://www.breakingbadapi.com/api/characters?category=Breaking+Bad");
 
-    // Parsing the data in json format
-    const data = await response.json();
+        // Parsing the data in json format
+        const data = await response.json();
 
-    // Sending a response
-    res.send(data);
+         // Sending a response
+        res.send(data);
+    }
+    catch(error) {
+        // Logging error if it occurs
+        console.log(error);
+    }
+
+    // Logging the status code
+    console.log(res.statusCode);
 });
 
 // Bonus
 app.get("/bettercallsaul", async (req, res) => {
-    // Fetching the data
-    const response = await fetch("https://www.breakingbadapi.com/api/characters?category=Better+Call+Saul");
+    try{
+        // Fetching the data
+        const response = await fetch("https://www.breakingbadapi.com/api/characters?category=Better+Call+Saul");
 
-    // Parsing the data in json format
-    const data = await response.json();
+        // Parsing the data in json format
+        const data = await response.json();
+    }
+    catch(error) {
+        // Logging error if it occurs
+        console.log(error);
+    }
+
+    // Logging the status code
+    console.log(res.statusCode);
 
     // Sending a response
     res.send(data);
